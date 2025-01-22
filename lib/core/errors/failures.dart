fbf8c738e72aef7ddecfd3ab4af5a001f8801d3b
NadaAlaa1 <nalaa839@gmail.com>
2025-01-22 23:54:20 +0200

    add failures

--- lib/core/errors/failures.dart
+++ lib/core/errors/failures.dart
@@ -1,3 +1,38 @@
-abstract class Failure {}
+import 'package:dio/dio.dart';
 
-class ServerFailure extends Failure {}
+abstract class Failure {
+  final String errMessage;
+
+  const Failure(this.errMessage);
+}
+
+class ServerFailure extends Failure {
+  ServerFailure(super.errMessage);
+
+  factory ServerFailure.fromDioError(DioException dioError) {
+    switch (dioError.type) {
+      case DioExceptionType.connectionTimeout:
+        return ServerFailure('Connection timeout with ApiServer');
+      case DioExceptionType.sendTimeout:
+        return ServerFailure('Send timeout with ApiServer');
+      case DioExceptionType.receiveTimeout:
+        return ServerFailure('Receive timeout with ApiServer');
+      case DioExceptionType.badCertificate:
+        break;
+      case DioExceptionType.badResponse:
+        break;
+      case DioExceptionType.cancel:
+        break;
+      case DioExceptionType.connectionError:
+        break;
+      case DioExceptionType.unknown:
+        break;
+    }
+  }
+
+  factory ServerFailure.fromResponse(int statusCode, dynamic response){
+    if(statusCode == 400 || statusCode == 401 || statusCode == 403){
+      return ServerFailure(response['error']);
+    }
+  }
+}
